cc542a80badc7fc653dbd86473f9a7e6b3013414
poketorena <poketorena@users.noreply.github.com>
2019-10-25 09:29:54 +0900

    [update] 2種類のコメントの練習

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
 public class Main {
-
+    /* コメントだよ */
+    // コメントだよ
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello world!!!");
     }
 }
