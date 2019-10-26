5310982464fd913ad074e240475006062562ace2
poketorena <poketorena@users.noreply.github.com>
2019-10-26 10:54:46 +0900

    [update] finalを使うと定数を作ることができる。

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,11 @@ public class Main {
     /* コメントだよ */
     // コメントだよ
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello world!!!");
+        final double TAX = 1.08;
+        int fax = 5;
+        System.out.println("5万円から4万円に値下げします。");
+        TAX = 4;
+        System.out.println("FAXの新価格（税込み）");
+        System.out.println(fax * TAX + "万円");
     }
 }
