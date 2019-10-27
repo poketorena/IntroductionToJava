b7c33f92c4577b5fd232567e0cb8fa9337ea85bd
poketorena <poketorena@users.noreply.github.com>
2019-10-27 20:24:08 +0900

    [update] 片方のオペランドがStringなら、他方もStringに変換して連結する。

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,17 +4,7 @@ public class Main {
     /* コメントだよ */
     // コメントだよ
     public static void main(String[] args) {
-        // int型リテラルをbyte型やshort型の変数に対して実害のない範囲で、
-        // 単純代入することは例外的に認められている。
-
-        // 符号付きbyte（-128～+127）
-        byte b1 = 127;
-        // これはだめ
-        // byte b2=128;
-
-        // 符号付きshort（-327678～+32767）
-        short s1 = 32767;
-        // これはだめ
-        // short s2=327678;
+        String message = "私の年齢は" + 23;
+        System.out.println(message);
     }
 }
