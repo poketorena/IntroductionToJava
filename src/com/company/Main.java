f23e8d0840f0fa9b0d459f0e2e730a6bf93b3222
poketorena <poketorena@users.noreply.github.com>
2019-10-27 20:00:25 +0900

    [update] エスケープシーケンスの使い方

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,6 @@ public class Main {
     /* コメントだよ */
     // コメントだよ
     public static void main(String[] args) {
-        boolean booleanVariable = true;
-        char charVariable = '駆';
-        double pi = 3.1_4;
-        long longPi = 3_14159__265853979L;
-        String message = "ミ_ナトの攻撃！敵に15ポイントのダメージを与えた。";
+        System.out.println("私の好きな記号は二重引用符（\"）です。");
     }
 }
