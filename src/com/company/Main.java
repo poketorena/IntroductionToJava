7ff717bb8bdf14011612a8bd4ff5e5663285f3d5
poketorena <poketorena@users.noreply.github.com>
2019-10-27 20:36:20 +0900

    [update] 乱数の使い方。

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,7 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        String age = "31";
-        int n = Integer.parseInt(age);
-        System.out.println("あなたは来年、" + (n + 1) + "歳になりますね。");
+        int r = new java.util.Random().nextInt(90);
+        System.out.println("あなたはたぶん、" + r + "歳ですね？");
     }
 }
