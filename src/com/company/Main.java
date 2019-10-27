0d9aaae741902b80282817eb05b2187cd79b9a3c
poketorena <poketorena@users.noreply.github.com>
2019-10-27 20:20:14 +0900

    [update] 代入時の自動型変換

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,17 @@ public class Main {
     /* コメントだよ */
     // コメントだよ
     public static void main(String[] args) {
-        System.out.println("私の好きな記号は二重引用符（\"）です。");
+        // int型リテラルをbyte型やshort型の変数に対して実害のない範囲で、
+        // 単純代入することは例外的に認められている。
+
+        // 符号付きbyte（-128～+127）
+        byte b1 = 127;
+        // これはだめ
+        // byte b2=128;
+
+        // 符号付きshort（-327678～+32767）
+        short s1 = 32767;
+        // これはだめ
+        // short s2=327678;
     }
 }
