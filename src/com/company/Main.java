d53edb2f5e0e08dd5e41a42f7b3340bef9b08896
poketorena <poketorena@users.noreply.github.com>
2019-10-27 20:33:21 +0900

    [update] 文字列を数字に変換する。

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,8 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 3;
-        int max = Math.max(a, b);
-        int min = Math.min(a, b);
-        System.out.println("最大値:" + max);
-        System.out.println("最小値:" + min);
+        String age = "31";
+        int n = Integer.parseInt(age);
+        System.out.println("あなたは来年、" + (n + 1) + "歳になりますね。");
     }
 }
