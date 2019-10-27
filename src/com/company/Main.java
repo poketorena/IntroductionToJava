588eb96970c3244a16127dfa8666181b7fd95fab
poketorena <poketorena@users.noreply.github.com>
2019-10-27 20:30:11 +0900

    [update] 最大値と最小値

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,12 @@
 package com.company;
 
 public class Main {
-    /* コメントだよ */
-    // コメントだよ
     public static void main(String[] args) {
-        String name = "すがわら";
-        System.out.print("私の名前は");
-        System.out.print(name);
+        int a = 5;
+        int b = 3;
+        int max = Math.max(a, b);
+        int min = Math.min(a, b);
+        System.out.println("最大値:" + max);
+        System.out.println("最小値:" + min);
     }
 }
