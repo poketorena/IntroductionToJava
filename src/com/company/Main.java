0a717972685d2b26fedb76d4f9d69c2b2a802248
poketorena <poketorena@users.noreply.github.com>
2019-10-30 05:48:26 +0900

    [update] while文の練習

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,16 +2,11 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ようこそ占いの館へ");
-        System.out.println("あなたの名前を入力してください");
-        String name = new java.util.Scanner(System.in).nextLine();
-        System.out.println("あなたの年齢を入力してください。");
-        String ageString = new java.util.Scanner(System.in).nextLine();
-        int age = Integer.parseInt(ageString);
-        int fortune = new java.util.Random().nextInt(4);
-        fortune++;
-        System.out.println("占いの結果が出ました！");
-        System.out.println(age + "歳の" + name + "さん、あなたの運気番号は" + fortune + "です");
-        System.out.println("1:大吉 2:中吉 3:吉 4:凶");
+        boolean doorClose = true;
+        while (doorClose) {
+            System.out.println("ノックする");
+            System.out.println("1分待つ");
+        }
+        System.out.println("トイレに入る");
     }
 }
