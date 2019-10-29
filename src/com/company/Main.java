af1a046303d3b31788271b28ff5e42503b4d5e49
poketorena <poketorena@users.noreply.github.com>
2019-10-30 06:44:58 +0900

    [update] 練習問題3-4（if文、else文で中括弧を省略するとこのようなバグを起こしやすい、中括弧は必ず付けるようにしよう）

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,19 +2,12 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        int seibetsu = 1;
-        int age = 18;
-
-        System.out.println("こんにちは。");
-
-        if (seibetsu == 0) {
-            System.out.println("私は男です。");
-            System.out.print(age);
-            System.out.println("歳です。");
-        } else {
-            System.out.println("私は女です。");
-        }
-
-        System.out.println("よろしくおねがいします。");
+        boolean tenki = true;
+        if (tenki) {
+            System.out.println("洗濯をします");
+            System.out.println("散歩に行きます");
+        } else
+            System.out.println("DVDを見ます");
+            System.out.println("寝ます");
     }
 }
