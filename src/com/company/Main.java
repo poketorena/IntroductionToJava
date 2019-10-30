3fa2028b9c16940052b76fa9a2f7cb3b9685c916
poketorena <poketorena@users.noreply.github.com>
2019-10-30 09:56:57 +0900

    [update] 練習問題4-2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,13 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        int[] points = new int[4];
-        double[] weights = new double[5];
-        boolean[] answers = new boolean[3];
-        String[] names = new String[3];
+        int[] moneyList = new int[]{121902, 8302, 55100};
+
+        for (int i = 0; i < moneyList.length; i++) {
+            System.out.println(moneyList[i]);
+        }
+        for (var money : moneyList) {
+            System.out.println(money);
+        }
     }
 }
