8d6aeec8568bb0657212251294a59f9eb11728c9
poketorena <poketorena@users.noreply.github.com>
2019-10-30 09:29:18 +0900

    [update] 配列の基本的な使い方

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,40 @@
 package com.company;
 
-import java.util.Random;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("数あてゲーム");
-        int ans = new Random().nextInt(10);
+        int[] scores1 = new int[5];
+        var scores2 = new int[5];
+        var scores3 = new int[]{1, 2, 3, 4, 5};
+        int[] scores4 = {10, 20, 30, 40, 50};
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println("0～9の数字を入力してください");
-            int num = new Scanner(System.in).nextInt();
-            if (num == ans) {
-                System.out.println("アタリ！");
-                break;
-            } else {
-                System.out.println("違います。");
-            }
+        int scoresCount1 = scores1.length;
+        int scoresCount2 = scores2.length;
+        int scoresCount3 = scores3.length;
+        int scoresCount4 = scores3.length;
+        System.out.println("scores1の要素数:" + scoresCount1);
+        System.out.println("scores2の要素数:" + scoresCount2);
+        System.out.println("scores3の要素数:" + scoresCount3);
+        System.out.println("scores4の要素数:" + scoresCount4);
+
+        for (int i = 0; i < scores1.length; i++) {
+            scores1[i] = i;
         }
-        System.out.println("ゲームを終了します");
+        for (int i = 0; i < scores2.length; i++) {
+            scores2[i] = (i + 1) * 111;
+        }
+
+        for (var score : scores1) {
+            System.out.println(score);
+        }
+        for (var score : scores2) {
+            System.out.println(score);
+        }
+        for (var score : scores3) {
+            System.out.println(score);
+        }
+        for (var score : scores4) {
+            System.out.println(score);
+        }
     }
 }
