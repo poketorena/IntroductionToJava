d617fb8b8e911d16a8fb258269050fe16ed1aee8
poketorena <poketorena@users.noreply.github.com>
2019-10-30 10:02:26 +0900

    [update] 練習問題4-4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,17 @@
 package com.company;
 
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] moneyList = new int[]{121902, 8302, 55100};
+        int[] numbers = new int[]{3, 4, 9};
+        System.out.println("1桁の数字を入力してください");
+        int input = new Scanner(System.in).nextInt();
 
-        for (int i = 0; i < moneyList.length; i++) {
-            System.out.println(moneyList[i]);
-        }
-        for (var money : moneyList) {
-            System.out.println(money);
+        for (var number : numbers) {
+            if (input == number) {
+                System.out.println("アタリ！");
+            }
         }
     }
 }
