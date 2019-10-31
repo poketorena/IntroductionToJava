9edb56374edeced493b1c738bd34d52aa9c5e61f
poketorena <poketorena@users.noreply.github.com>
2019-10-31 19:08:54 +0900

    [update] 練習問題5-1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,18 +3,20 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-    public static void incArray(int[] array) {
-        for (var i = 0; i < array.length; i++) {
-            array[i]++;
-        }
+    public static void introduceOneself() {
+        String name = "Java仙人";
+        int age = 999;
+        double height = 123.45;
+        char gender = '男';
+
+        System.out.println("こんにちは、私の名前は" + name + "です。");
+        System.out.println("年齢は" + age + "です。");
+        System.out.println("身長は" + height + "です。");
+        System.out.println("性別は" + gender + "です。");
+        System.out.println("よろしくおねがいします。");
     }
 
     public static void main(String[] args) {
-        int[] array = {1, 2, 3};
-        incArray(array);
-
-        for (var i : array) {
-            System.out.println(i);
-        }
+        introduceOneself();
     }
 }
