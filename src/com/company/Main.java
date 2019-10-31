0c1cb6a8dc8664281dac5bcbd13cfe93ad9b733f
poketorena <poketorena@users.noreply.github.com>
2019-10-31 19:13:45 +0900

    [update] 練習問題5-2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,20 +3,13 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-    public static void introduceOneself() {
-        String name = "Java仙人";
-        int age = 999;
-        double height = 123.45;
-        char gender = '男';
-
-        System.out.println("こんにちは、私の名前は" + name + "です。");
-        System.out.println("年齢は" + age + "です。");
-        System.out.println("身長は" + height + "です。");
-        System.out.println("性別は" + gender + "です。");
-        System.out.println("よろしくおねがいします。");
+    public static void email(String title, String address, String text) {
+        System.out.println(address + "に、以下のメールを送信しました");
+        System.out.println("件名：" + title);
+        System.out.println("本文：" + text);
     }
 
     public static void main(String[] args) {
-        introduceOneself();
+        email("自己紹介", "sample@hoge.com", "こんにちは");
     }
 }
