4383853f8f8a2bdb196a3b113b536ebae3132824
poketorena <poketorena@users.noreply.github.com>
2019-11-04 00:51:43 +0900

    [update] 練習問題6-3

---
+++ src/comment/Zenhan.java
@@ -0,0 +1,11 @@
+package comment;
+
+public class Zenhan {
+    public static void doWatusa() {
+        System.out.println("きなこでござる。食えませんがの。");
+    }
+
+    public static void doTogame() {
+        System.out.println("この老いぼれの目はごまかせませんぞ。");
+    }
+}
