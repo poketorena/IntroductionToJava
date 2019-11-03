9736d038296094a6a0588822ef02391347db32d8
poketorena <poketorena@users.noreply.github.com>
2019-11-03 21:25:00 +0900

    [update] 複数のファイルを用いた開発

--- src/com/company/Calc.java
+++ src/com/company/Calc.java
@@ -6,16 +6,8 @@ public class Calc {
     public static void main(String[] args) {
         int a = 10;
         int b = 2;
-        int total = tasu(a, b);
-        int delta = hiku(a, b);
+        int total = CalcLogic.tasu(a, b);
+        int delta = CalcLogic.hiku(a, b);
         System.out.println("足すと" + total + "引くと" + delta);
-    }
-
-    private static int tasu(int a, int b) {
-        return a + b;
-    }
-
-    private static int hiku(int a, int b) {
-        return a - b;
     }
 }
