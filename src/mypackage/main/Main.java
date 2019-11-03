76f5018d91735557ee89a9e919736faede508943
poketorena <poketorena@users.noreply.github.com>
2019-11-03 22:17:12 +0900

    [update] API利用の例

---
+++ src/mypackage/main/Main.java
@@ -0,0 +1,13 @@
+package mypackage.main;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] heights = {172, 149, 152, 191, 155};
+        Arrays.sort(heights);
+        for (int h : heights) {
+            System.out.println(h);
+        }
+    }
+}
