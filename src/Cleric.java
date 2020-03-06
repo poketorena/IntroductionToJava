5ba738222575b94fc45085dcb262fb52504daf47
poketorena <poketorena.develop@gmail.com>
2020-03-06 13:08:09 +0900

    [update] 練習問題8-4

--- src/Cleric.java
+++ src/Cleric.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Cleric {
     String name;
     int hp = 50;
@@ -13,5 +15,12 @@ public class Cleric {
         this.mp -= 5;
         this.hp = this.MAX_HP;
         System.out.println("HPを最大値まで回復しました！");
+    }
+
+    public int pray(int sec) {
+        int mpRecoveryAmount = sec + new Random().nextInt(2);
+        int actualMpRecoveryAmount = mpRecoveryAmount % this.MAX_MP;
+        this.mp += actualMpRecoveryAmount;
+        return actualMpRecoveryAmount;
     }
 }
