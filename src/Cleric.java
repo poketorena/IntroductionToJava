e36f8027d11db20ddfd393c516ed32efd21e6e50
poketorena <poketorena.develop@gmail.com>
2020-03-06 12:58:51 +0900

    [update] 練習問題8-3

--- src/Cleric.java
+++ src/Cleric.java
@@ -4,4 +4,14 @@ public class Cleric {
     final int MAX_HP = 50;
     int mp = 10;
     final int MAX_MP = 10;
+
+    public void selfAid() {
+        if (this.mp < 5) {
+            System.out.println("MPが足りません！");
+            return;
+        }
+        this.mp -= 5;
+        this.hp = this.MAX_HP;
+        System.out.println("HPを最大値まで回復しました！");
+    }
 }
