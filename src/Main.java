dae333ea417f57316e7bf48602086a307a91830d
poketorena <poketorena.develop@gmail.com>
2020-03-08 23:33:20 +0900

    [update] 練習問題12-1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        Book b = new Book("hoge", 1000, "Blue", "123465789");
+        Computer c = new Computer("fuga", 5000, "Red", "hey");
+        System.out.println(b.getName());
+        System.out.println(c.getName());
     }
 }
