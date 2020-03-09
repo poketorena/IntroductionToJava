3c6fd11da0447a417474b45bb7ce6fdf5991c273
poketorena <poketorena.develop@gmail.com>
2020-03-09 19:09:14 +0900

    [update] 練習問題14-1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        int i1 = 15;
-        // 明示的に書く場合
-        Integer i2 = Integer.valueOf(i1);
-        // 暗黙の型変換を使う場合（AutoBoxing）
-        Integer i3 = i1;
-        // 明示的に書く場合
-        int i4 = i2.intValue();
-        // 暗黙の型変換を使う場合（AutoUnboxing）
-        int i5 = i2;
+        Date currentDate = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(currentDate);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        day += 100;
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        Date futureDate = calendar.getTime();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("西暦yyyy年MM月dd日");
+        String formatedStr = simpleDateFormat.format(futureDate);
+        System.out.println(formatedStr);
     }
 }
