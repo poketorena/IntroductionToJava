c5cef767538730b9a6810240a1b393bd33fa5dea
poketorena <poketorena.develop@gmail.com>
2020-03-09 18:52:44 +0900

    [update] Date型と人間にとって読みやすい日時の相互変換

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,14 @@
-import java.util.Calendar;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Main {
-    public static void main(String[] args) {
-        // 現在の年を表示する
+    public static void main(String[] args) throws Exception {
+        // 本日の日時を表示する
         Date now = new Date();
-        Calendar c = Calendar.getInstance();
-        c.setTime(now);
-        int y = c.get(Calendar.YEAR);
-        System.out.println("今年は" + y + "年です。");
-
-        // 指定した日のDate型の値を得る
-        c.set(2010, 8, 22, 1, 23, 45);
-        // Calendarを用いて「月」の情報を取得・設定する場合、1~12ではなく0~11で指定することになっているため注意が必要。
-        // 以下のようにCalendar型に定義されている定数を使うと間違えにくいので積極的に定数を使おう！
-        // c.set(2010, Calendar.SEPTEMBER, 22, 1, 23, 45);
-
-        c.set(Calendar.YEAR, 2011);
-        Date past = c.getTime();
+        SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        String s = f.format(now);
+        System.out.println(s);
+        // 指定日時の文字列を解析し、Date型として得る
+        Date d = f.parse("2011/09/22 01:23:45");
     }
 }
