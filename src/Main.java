300365803a94e27ca4d6bd1e0ddda3ea35741245
poketorena <poketorena.develop@gmail.com>
2020-03-11 19:01:25 +0900

    [update] FileReaderの使い方（Stream）

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,34 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        throw new IOException();
+    public static void main(String[] args) {
+        String filename = "test.txt";
+        FileReader fr = null;
+        char c1 = 'z';
+        char c2 = 'z';
+        try {
+            fr = new FileReader(filename);
+            try {
+                c1 = (char) fr.read();// 1文字読む
+                c2 = (char) fr.read();// 1文字読む
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fr != null) {
+                    fr.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("最初の1文字 : " + c1);
+        System.out.println("次の1文字 : " + c2);
     }
 }
