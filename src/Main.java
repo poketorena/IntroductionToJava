d2ff6e6a63454099a9f4b678fdf119171b96c466
poketorena <poketorena.develop@gmail.com>
2020-03-11 19:20:22 +0900

    [update] try-with-resources文の練習
    例外処理が減って、ソースコードが読みやすくなる。
    積極的に使おう。
    C#のusingステートメントと似てる

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,9 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         String filename = "test.txt";
-        FileWriter fileWriter = null;
         char c1 = 'z';
         char c2 = 'z';
-        try {
-            fileWriter = new FileWriter(filename);
+        try (FileWriter fileWriter = new FileWriter(filename)) {
             try {
                 fileWriter.write('そ');// 1文字書く（上書き）
                 fileWriter.write('れ');// 1文字書く（上書き）
@@ -17,14 +15,6 @@ public class Main {
             }
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (fileWriter != null) {
-                    fileWriter.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
         System.out.println("書き込み成功！");
     }
