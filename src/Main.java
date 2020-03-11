06977b78a5d55e8c8ee8c5804a27dc8710f1a47a
poketorena <poketorena.develop@gmail.com>
2020-03-11 18:33:33 +0900

    [update] 練習問題15-1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        BankAccount bankAccount1 = new BankAccount("4649", 1592);
-        BankAccount bankAccount2 = new BankAccount(" 4649", 1592);
-        System.out.println(bankAccount1);
-        System.out.println(bankAccount2);
-
-        if (bankAccount1.equals(bankAccount2)) {
-            System.out.println("同じだよ。");
-        } else {
-            System.out.println("違うよ。");
-        }
+        String s = null;
+        System.out.println(s.length());
     }
 }
