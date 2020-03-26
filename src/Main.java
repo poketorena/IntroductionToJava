fe59eb8cabbc9a0a09530cbfc8a41be297fe9096
poketorena <poketorena.develop@gmail.com>
2020-03-26 17:22:57 +0900

    [update] swtch文の使い方
    breakの書き忘れによるフォールスルーとEnumが状態nullを取ることができることに注意（詳しくはソースコード参照）

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,36 @@
 import java.time.LocalDateTime;
+import java.time.Month;
 
 public class Main {
     public static void main(String... args) {
-        int second = LocalDateTime.now().getSecond();
+        Month month = LocalDateTime.now().getMonth();
 
-        if (second % 2 == 0) {
-            System.out.println(second + "は偶数です");
-        } else {
-            System.out.println(second + "は奇数です");
+        //JavaのEnumはnullを代入することができる
+        Month nullDayo = null;
+
+        switch (month) {
+            case MARCH:
+                // C#は空でないcaseブロックには必ずbreakを書く必要があるが、
+                // Javaは空でないブロックにbreakを書き忘れた場合（フォールスルー）もエラーにならない！！！
+                // バグの元なので気をつけよう！
+                var tmp = 123;
+            case APRIL:
+            case MAY:
+                System.out.println(month + "は春です");
+                break;
+            case JUNE:
+            case JULY:
+            case AUGUST:
+                System.out.println(month + "は夏です");
+                break;
+            case SEPTEMBER:
+            case OCTOBER:
+            case NOVEMBER:
+                System.out.println(month + "は秋です");
+                break;
+            default:
+                System.out.println(month + "は冬です");
+                break;
         }
     }
 }
