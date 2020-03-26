24fe1d04fdf19c71fa76d6c9eb73fad93101c68b
poketorena <poketorena.develop@gmail.com>
2020-03-26 16:29:39 +0900

    [update] ビットシフトを使って、32bitの値から上位16bitを取り出す

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
 public class Main {
     public static void main(String... args) {
         int number = 0x12345678;
-        System.out.println("int number = 0x12345678");
-
-        int lower = number & 0x0000ffff;
-        System.out.println("int lower = number & 0x0000ffff");
-
-        System.out.println("16 ^ 0 * 8 + 16 ^ 1 * 7 + 16 ^ 2 * 6 + 16 ^ 3 * 5 = 22136");
-        System.out.println("lower(10進数) = " + lower);
-        System.out.printf("lower(16進数) = %x\n", lower);
+        int higher = number >> 16;
+        System.out.printf("higher=%x\n", higher);
     }
 }
