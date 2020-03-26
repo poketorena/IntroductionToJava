9e97b834c2ddcf4094e2b348eb8426f325f445be
poketorena <poketorena.develop@gmail.com>
2020-03-26 16:26:30 +0900

    [update] 32bitの値から、下位16bitを取り出す処理

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String... args) {
-        // Pythonでは「3.333…」だが、Javaでは「3」になる
-        System.out.println(10 / 3);
+        int number = 0x12345678;
+        System.out.println("int number = 0x12345678");
+
+        int lower = number & 0x0000ffff;
+        System.out.println("int lower = number & 0x0000ffff");
+
+        System.out.println("16 ^ 0 * 8 + 16 ^ 1 * 7 + 16 ^ 2 * 6 + 16 ^ 3 * 5 = 22136");
+        System.out.println("lower(10進数) = " + lower);
+        System.out.printf("lower(16進数) = %x\n", lower);
     }
 }
