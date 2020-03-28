f01058a3426fdea0654186794a736cfcb9520981
poketorena <poketorena.develop@gmail.com>
2020-03-29 02:27:39 +0900

    [update] プリミティブ型のラッパークラスの代表的なメソッド

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
 public class Main {
     public static void main(String... args) {
-        System.out.println("Byte: { SIZE(bit): " + Byte.SIZE
-                + ", BYTES: " + Byte.BYTES
-                + ", MIN: " + Byte.MIN_VALUE
-                + ", MAX:" + Byte.MAX_VALUE);
+        // int -> Integer
+        Integer num01 = new Integer(10);// 新たなオブジェクトを生成するので効率が悪い
+        Integer num02 = Integer.valueOf(10); // キャッシュされたオブジェクトを返す
 
-        System.out.println("Integer: { SIZE(bit): " + Integer.SIZE
-                + ", BYTES: " + Integer.BYTES
-                + ", MIN: " + Integer.MIN_VALUE
-                + ", MAX:" + Integer.MAX_VALUE);
+        // Integer -> int
+        int num03 = num02.intValue();
+
+        // String -> Integer
+        Integer num04 = new Integer("10"); // 新たなオブジェクトを生成するので効率が悪い
+        Integer num05 = Integer.valueOf("10");// キャッシュされたオブジェクトを返す
+        Integer num06 = Integer.valueOf("11", 2);// 基数（radix）を指定。この場合の値は3
+
+        // String -> int
+        int num07 = Integer.parseInt("10");
+        int num08 = Integer.parseInt("11", 2);// 基数（radix）を指定。このの値は3
+
+        // int -> String
+        String num09 = Integer.toString(10);
+
+        // Integer -> String
+        String num10 = num01.toString();
     }
 }
