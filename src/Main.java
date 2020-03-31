e5432ef6f69bb877238da286dbaad0a9efee646b
poketorena <poketorena.develop@gmail.com>
2020-03-31 16:50:33 +0900

    [update] 配列内の要素の検索（二分探索）

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,21 @@ import java.util.Comparator;
 
 public class Main {
     public static void main(String... args) {
-        Student[] students = {
-                new Student("Ken", 100),
-                new Student("Shin", 60),
-                new Student("Takuya", 80)
-        };
+        int[] array = {1, 1, 4, 5, 7, 8, 11, 12, 17, 21, 24};
 
-        Arrays.sort(students, (o1, o2) -> Integer.compare(o2.getScore(), o1.getScore()));
-        for (Student student : students) {
-            System.out.println(student.getName() + ":" + student.getScore());
-        }
+        // 配列内の検索（二分探索）
+        // 二分探索のため、配列は事前に昇順にソートされている必要がある
+        int found = Arrays.binarySearch(array, 5);// ①「5」という数字を検索
+        System.out.println(found);
+
+        int notFound = Arrays.binarySearch(array, 6);// ②「6」という数字を検索
+
+        // 見つからなかったときは負の値が返ってくる
+        // 見つからなかったときに返ってくる値は、
+        // 「もしその要素が入るとすれば何番目となるか」という数値に
+        // マイナス符号を付け、さらに1引いた数になっている。
+        // この場合はもし「6」が入るなら4番目（インデックス）なので、
+        // -4から1を引いた-5が返ってきている。
+        System.out.println(notFound);
     }
 }
