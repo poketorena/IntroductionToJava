49911387a6e3b8f1f862276f0d0c95657aed7c4f
poketorena <poketorena.develop@gmail.com>
2020-03-31 12:24:50 +0900

    [update] List、ArrayListとダイヤモンドオペレータ

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ import java.util.List;
 
 public class Main {
     public static void main(String... args) {
-        List list = new ArrayList();
+//        List<String> list = new ArrayList<String>(); // Java 5.0 Java 6での書き方
+        List<String> list = new ArrayList<>(); // Java 7以降ならダイヤモンドオペレータが使用可能
         list.add("Java");
-        String element = (String) list.get(0);// キャストが必要！
+        String element = list.get(0);// キャストが必要！
         System.out.println(element);
     }
 }
