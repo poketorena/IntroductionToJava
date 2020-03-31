9772e80abf6be7ec45753e15d6fae124ae68cbeb
poketorena <poketorena.develop@gmail.com>
2020-03-31 11:36:23 +0900

    [update] hashCodeメソッドをオーバーライドし忘れたときに発生する問題（hashCodeをオーバーライドした場合）

--- src/Employee.java
+++ src/Employee.java
@@ -31,4 +31,13 @@ public class Employee {
         }
         return true;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + employeeNo;
+        result = prime * result + ((employeeName == null) ? 0 : employeeName.hashCode());
+        return result;
+    }
 }
