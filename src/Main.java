5a6b94976820bff425e0654b83488b385031b501
poketorena <poketorena.develop@gmail.com>
2020-03-31 16:56:52 +0900

    [update] メソッドの引数を配列から可変長引数に書き換えると可読性が上がる？

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,16 @@
-import java.util.Arrays;
-import java.util.Comparator;
-
 public class Main {
     public static void main(String... args) {
-        int[] array = {1, 1, 4, 5, 7, 8, 11, 12, 17, 21, 24};
+        log("ユーザを登録しました", "userName", "Ken");
+        log("エラーが発生しました", "Cannot load file");
+        log("処理を終了しました");
 
-        // 配列内の検索（二分探索）
-        // 二分探索のため、配列は事前に昇順にソートされている必要がある
-        int found = Arrays.binarySearch(array, 5);// ①「5」という数字を検索
-        System.out.println(found);
+    }
 
-        int notFound = Arrays.binarySearch(array, 6);// ②「6」という数字を検索
-
-        // 見つからなかったときは負の値が返ってくる
-        // 見つからなかったときに返ってくる値は、
-        // 「もしその要素が入るとすれば何番目となるか」という数値に
-        // マイナス符号を付け、さらに1引いた数になっている。
-        // この場合はもし「6」が入るなら4番目（インデックス）なので、
-        // -4から1を引いた-5が返ってきている。
-        System.out.println(notFound);
+    private static void log(String message, String... args) {
+        System.out.println(message);
+        System.out.println("パラメータ：");
+        for (String arg : args) {
+            System.out.println(arg);
+        }
     }
 }
