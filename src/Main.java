50ea5264a176c809abcbb8038746e88892424130
poketorena <poketorena.develop@gmail.com>
2020-03-31 12:48:17 +0900

    [update] メソッドに対するジェネリクスとメソッド参照

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String... args) {
-        GenericStack<String> genStack = new GenericStack<>();
-
-        genStack.push("Scala");
-        genStack.push("Groovy");
-        genStack.push("Java");
-
-        String genElement = genStack.pop();
-
-        if (genElement != null) {
-            System.out.println(genElement);
-        }
-
-        GenericStack<Integer> genStack2 = new GenericStack<>();
-
-        genStack2.push(100);
-        genStack2.push(200);
-
-        Integer genElement2 = genStack2.pop();
-
-        if (genElement2 != null) {
-            System.out.println(genElement2);
-        }
+        List<String> strList = new ArrayList<>();
+        strList.add("Java");
+        strList.add("Groovy");
+        GenericStack<String> gstack = GenericStackUtil.as(strList);
     }
 }
