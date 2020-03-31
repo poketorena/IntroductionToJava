543a508e16a5cd03443c45ba61d680dbd2ac1120
poketorena <poketorena.develop@gmail.com>
2020-03-31 17:10:00 +0900

    [update] Arrays.asListで作ったリストは要素の追加・変更・削除をすることができない！

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String... args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(62);
-        list.add(31);
-        list.add(1);
-        list.add(54);
-        list.add(31);
+        List<Integer> integerList = Arrays.asList(1, 62, 31, 1, 54, 31);
+
+        // Arrays.asListで作ったリストは要素の追加・変更・削除をすることができない！
+         integerList.add(999);
+
+        for (var element : integerList) {
+            System.out.println(element);
+        }
     }
 }
