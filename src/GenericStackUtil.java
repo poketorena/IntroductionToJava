eadb3b922b4025c56b6a6d207078bd920dbe9e9d
poketorena <poketorena.develop@gmail.com>
2020-03-31 16:23:48 +0900

    [update] 配列のソートと配列のtoString

--- src/GenericStackUtil.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public class GenericStackUtil {
-    public static <T> GenericStack<T> as(List<T> list) {
-        GenericStack<T> stack = new GenericStack<>();
-        list.forEach(stack::push); // メソッド参照
-        return stack;
-    }
-}
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,9 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String... args) {
-        NumberStack<Integer> intStack = new NumberStack<>();// IntegerクラスはNumberクラス
-
-        NumberStack<Long> longStack = new NumberStack<>();// LongクラスはNumberクラスの子クラス
-
-        // StringはNumberクラスの小クラスではないためコンパイルエラー
-        // NumberStack<String> strStack = new NumberStack<String>();
-        intStack.push(100);
-        intStack.push(200);
-
-        Integer numElement = intStack.pop();
-
-        if (numElement != null) {
-            System.out.println(numElement);
-        }
+        int[] array = {3, 1, 13, 2, 8, 5, 1};
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
     }
 }
