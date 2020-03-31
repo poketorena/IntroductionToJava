ebac764ac2b1c185f2eed239481886250228ced7
poketorena <poketorena.develop@gmail.com>
2020-03-31 16:27:28 +0900

    [update] ラムダ式を使って配列を降順にソートする

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,7 @@ import java.util.Comparator;
 public class Main {
     public static void main(String... args) {
         Integer[] array = {3, 1, 13, 2, 8, 5, 1};
-        Comparator<Integer> c = new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o2.compareTo(o1);
-            }
-        };
+        Comparator<Integer> c = (o1, o2) -> o2.compareTo(o1);
 
         Arrays.sort(array, c);
         System.out.println(Arrays.toString(array));
